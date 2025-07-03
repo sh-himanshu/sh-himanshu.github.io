310c307fe4430cec7ef5c0a4fa80ee1cd3bd8076
Himanshu Sharma <himanshu.sharma3@birdeye.com>
2025-07-03 20:25:04 +0530

    fix build error by adding demo generateStaticParams

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,5 +1,24 @@
-const page = () => {
-  return <div>page</div>;
-};
+import { Post } from '@/types';
 
-export default page;
+export async function generateStaticParams() {
+  const posts = await fetch('https://jsonplaceholder.typicode.com/posts').then((res) => res.json());
+
+  return posts.map((post: Post) => ({
+    id: post.id.toString(),
+  }));
+}
+
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const post: Post = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then((res) => {
+    if (!res.ok) return null;
+    return res.json();
+  });
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+}
