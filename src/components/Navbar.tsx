bc23aecaa80094eeb62c607a0a4dbf78a6dab7b0
Himanshu Sharma <himanshu.sharma3@birdeye.com>
2025-07-03 20:59:53 +0530

    add dark mode toggle

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,12 @@
+import { ModeToggle } from '@/components/Theme/ModeToggle';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <ModeToggle />
+      Navbar
+    </div>
+  );
 };
 
 export default Navbar;
