c3547d9078c3d12e27a0350eec1312eb44ecdcec
Himanshu Sharma <himanshu.sharma3@birdeye.com>
2025-07-03 22:24:54 +0530

    fix js not working

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/sh-himanshu.github.io' : '',
+  // basePath: isProd ? '/sh-himanshu.github.io' : '',
+  // assetPrefix: isProd ? '/sh-himanshu.github.io' : '',
   images: {
     unoptimized: true,
   },
